39561c12f8dec60fa9db9ed3a7c7d92107bd90aa
SamyBidar2 <samybidarpro@gmail.com>
2024-05-02 11:53:19 +0200

    ajout kit fontawesome + début Rating

--- src/components/Rating/index.jsx
+++ src/components/Rating/index.jsx
@@ -1,15 +1,20 @@
 import logementdatas from '../../datas/Logementdatas.json'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 import './index.scss'
 
 export const Rating = ({id}) => {
     const place = logementdatas.find(data => data.id === id);
-    const tags = place.tags;
+    
 
     return (
-            <div className='tags-container'>
-                {tags.map((tag, index) => (
-                    <div className='tag' key= {`${place.id}-${index}`}> {tag} </div>
-                ))}
-            </div>
+        <>
+        <FontAwesomeIcon icon={faStar} />
+        <FontAwesomeIcon icon={faStar} />
+        <FontAwesomeIcon icon={faStar} />
+        <FontAwesomeIcon icon={faStar} />
+        <FontAwesomeIcon icon={faStar} />
+        </>
+        
     )
 }
