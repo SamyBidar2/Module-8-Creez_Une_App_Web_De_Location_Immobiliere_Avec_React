a051bba90ba6ae36464aad1e3abe5ed0c39a6cf7
SamyBidar2 <samybidarpro@gmail.com>
2024-05-02 11:44:20 +0200

    corerctions Fiche logement

--- src/components/Details/index.jsx
+++ src/components/Details/index.jsx
@@ -1,6 +1,7 @@
 import logementdatas from '../../datas/Logementdatas.json'
 import { Collapse } from '../Collapse'
 import { Tags } from '../Tags'
+import { Rating } from '../Rating'
 import './index.scss'
 
 export const Details = ({id}) => {
@@ -12,13 +13,13 @@ export const Details = ({id}) => {
             <div className='details-container'>
                 <div className='place-title'>{place.title}</div>
                 <div className='place-location'>{place.location}</div>
+                <Rating id={id}/>
                 <Tags id={id}/>
                 <div className='collapse-container'>
                     <Collapse Collapsetitle={"Description"} Collapsetext={description} Size={'medium'}/>
                     <Collapse Collapsetitle={"Équipements"} Collapsetext={equipement} Size={'medium'}/>
                 </div>
                 
-
             </div>
     )
 }
